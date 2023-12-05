238591f7379ba3f391ee8f7b37d771c7c609acbb
seungchan woo <tmdcksqkd123@naver.com>
2023-12-05 19:09:14 +0900

    repeat: 모달 팝업 띄우기 기능 완료
    1.js 모달 팝업 띄우기 작성.
    2.css 모달 팝업에 필요한 로직 추가.

---
+++ modal/script.js
@@ -0,0 +1,18 @@
+(function () {
+    const get = (target) => {
+        return document.querySelector(target);
+    };
+
+    const $button = get(`.modal_open_button`);
+    const $modal = get(`.modal`);
+    const $body = get(`body`);
+
+    const toggleModal = () => {
+        $modal.classList.toggle("show");
+        $body.classList.toggle("scroll_lock");
+    };
+
+    $button.addEventListener("click", () => {
+        toggleModal();
+    });
+})();
